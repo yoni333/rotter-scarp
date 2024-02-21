f1c1184f51f2c180dccfbabbff5a1fbb85b70553
yehonatan yehezkel <yoni333@gmail.com>
2024-02-21 09:16:04 +0200

    better filesave

--- src/services/save-files.js
+++ src/services/save-files.js
@@ -8,22 +8,22 @@ class saveToFiles {
     }
 
 
-    saveToFiles(simpleArray, nestedJSON, nestedYAML, targetUrl) {
+    saveToFiles(simpleArray, nestedJSON, nestedYAML, targetUrl,output= 'output') {
 
         console.log("saveToFiles ", targetUrl);
         // Save results to files (optional)
-        const outputDir = path.join(__dirname, '..', '..', 'output');
+        const outputDir = path.join(__dirname, '..', '..', output);
 
         fs.mkdir(outputDir, { recursive: true }, (err) => {
             if (err) throw err;
         });
-        fs.writeFileSync(outputDir + "/" + 'comments_simple_array.json', simpleArray);
+        fs.writeFileSync( path.join(outputDir , 'comments_simple_array.json'), simpleArray);
         //how to write athe aboe 2 lines in 1 line  
 
 
 
-        fs.writeFileSync('comments_nested_json.json', nestedJSON);
-        fs.writeFileSync('comments_nested_yaml.yaml', nestedYAML);
+        fs.writeFileSync( path.join(outputDir , 'comments_nested_json.json', nestedJSON));
+        fs.writeFileSync( path.join(outputDir ,  'comments_nested_yaml.yaml', nestedYAML));
 
     }
 }
